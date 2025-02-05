c513cbc2c47cad6d2968ba41b1023840a5ffc7cf
Widarft <adibseptiawan@gmail.com>
2025-02-05 15:12:48 +0700

    Update HeroSection.jsx

--- ai-customercare/src/features/landing-page/components/HeroSection.jsx
+++ ai-customercare/src/features/landing-page/components/HeroSection.jsx
@@ -6,6 +6,7 @@ const HeroSection = () => {
     <div className=" flex items-center justify-center">
       <img src={logo} alt="logo" />
       <h1 className="font-extrabold text-center text-7xl">Hero Section</h1>
+      <p className="flex col">Branch Dev</p>
     </div>
   );
 };
